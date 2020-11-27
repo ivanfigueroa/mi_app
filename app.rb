0e4064a1159c1e9ecf6ee6f07245d17af2d80c3c
Mario Figueroa <mysw1@hotmail.com>
2020-11-27 15:14:21 +0000

    Added lib behaviour to controller

--- app.rb
+++ app.rb
@@ -3,15 +3,18 @@ require './config'
 require './lib/ahorcado.rb'
 
 get '/' do
+    ahorcado = Ahorcado.new('ana')
+    session[:ahorcado] = ahorcado
     erb :index
 end
 
 post '/adivinar' do
-    ahorcado = Ahorcado.new('ana')
-    if ahorcado.arriesgar(params[:palabra])
+    
+    if session[:ahorcado].arriesgar(params[:palabra])
         session[:feedback] = "Sí está en la palabra"
     else
         session[:feedback] = "No está en la palabra"
     end
+    session[:errores] = session[:ahorcado].errores
     erb :index
 end
--- spec/ahorcado_spec.rb
+++ spec/ahorcado_spec.rb
@@ -14,13 +14,13 @@ describe Ahorcado do
 	it "el usuario no acierta con la letra 'x' y suma un error" do
 		ahorcado = Ahorcado.new("Ana")
 		ahorcado.arriesgar('x')
-		expect(ahorcado.errores).to eq 1
+		expect(ahorcado.errores).to eq "1"
 	end
 
 	it "el usuario no acierta con la letra 'x' ni 'z' y suma dos errores" do
 		ahorcado = Ahorcado.new("Ana")
 		ahorcado.arriesgar('x')
 		ahorcado.arriesgar('z')
-		expect(ahorcado.errores).to eq 2
+		expect(ahorcado.errores).to eq "2"
 	end
 end
